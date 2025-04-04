652b120258dab2186503713d41ba00e0aca41829
Stefano Gaspardone <s337201@studenti.polito.it>
2025-04-04 21:04:21 +0200

    Week 6 update

--- week 7/react-qa/src/App.jsx
+++ week 7/react-qa/src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Question, Answer } from "./QAModels.mjs";
+import { Answer, Question } from "./QAModels.mjs";
 import NavHeader from "./components/NavHeader";
 import { Container } from 'react-bootstrap';
 import QuestionDescription from './components/QuestionDescription';
@@ -8,7 +8,6 @@ import { useState } from 'react';
 
 const fakeQuestion = new Question(1, 'Is JavaScript better than Python?', 'luigi.derussis@polito.it', '2024-02-07');
 fakeQuestion.init();
-
 const fakeAnswers = fakeQuestion.getAnswers();
 
 function App() {
@@ -16,13 +15,35 @@ function App() {
   const [answers, setAnswers] = useState(fakeAnswers);
 
   const voteUp = (answerId) => {
-    console.log(answerId);
+    setAnswers(oldAnswers => {
+      return oldAnswers.map(ans => {
+        if(ans.id === answerId)
+          // ritorno una nuova, aggiornata, risposta
+          return new Answer(ans.id, ans.text, ans.email, ans.date, ans.score +1);
+        else
+          return ans;
+      });
+    });
+  }
 
-    setAnswers(oldAnswers => oldAnswers.map(ans => {
-      if(ans.id === answerId) return new Answer(ans.id, ans.text, ans.email, ans.date, ans.score + 1);
+  const addAnswer = (answer) => {
+    setAnswers(oldAnswers => {
+      const newId = Math.max(...oldAnswers.map(ans => ans.id)) + 1;
+      const newAnswer = new Answer(newId, answer.text, answer.email, answer.date, 0);
+      return [...oldAnswers, newAnswer];
+    });
+  }
 
-      return ans;
-    }));
+  const updateAnswer = (answer) => {
+    setAnswers(oldAnswers => {
+      return oldAnswers.map((ans) => {
+        if(ans.id === answer.id) {
+          return new Answer(answer.id, answer.text, answer.email, answer.date, ans.score);
+        }
+        else
+          return ans;
+      });
+    });
   }
 
   return (
@@ -30,7 +51,7 @@ function App() {
       <NavHeader questionNum={question.id} />
       <Container fluid className='mt-3'>
         <QuestionDescription question={question} />
-        <Answers answers={answers} voteUp = {voteUp}></Answers>
+        <Answers answers={answers} voteUp={voteUp} addAnswer={addAnswer} updateAnswer={updateAnswer}></Answers>
       </Container>
     </>
   )
--- week 7/react-qa/src/components/AnswerComponents.jsx
+++ week 7/react-qa/src/components/AnswerComponents.jsx
@@ -1,18 +1,32 @@
-/* eslint-disable react/prop-types */
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Row, Col, Table, Button } from "react-bootstrap";
-import FormAnswer from './FormAnswer';
+import AnswerForm from './AnswerForm';
+import { useState } from 'react';
 
-function Answers (props) {
+function Answers(props) {
+  const [mode, setMode] = useState('view');
+  const [editableAnswer, setEditableAnswer] = useState();
+
+  const handleEdit = (answer) => {
+    setEditableAnswer(answer);
+    setMode('edit');
+  }
+
   return(
     <>
     <Row>
-      <Col as='h2'>Answers:</Col>
+      <Col as='h2'>Answers ({props.answers.length}):</Col>
     </Row>
     <Row>
       <Col lg={10} className="mx-auto">
-        <AnswerTable answers={props.answers} voteUp = {props.voteUp}/>
-        <FormAnswer/>
+        <AnswerTable answers={props.answers} voteUp={props.voteUp} handleEdit={handleEdit}></AnswerTable>
+        
+        {mode === 'view' && <Button variant='primary' onClick={() => {setMode('add');}}>Add</Button>}
+        
+        {mode === 'add' && <AnswerForm addAnswer={(answer) => {props.addAnswer(answer); setMode('view');}} cancel={()=> setMode('view')} mode={mode}/>}
+
+        {mode === 'edit' && <AnswerForm key={editableAnswer.id} answer={editableAnswer} updateAnswer={(answer) => {props.updateAnswer(answer); setMode('view');}} cancel={()=> setMode('view')} mode={mode}/>}
+
       </Col>
     </Row>
     </>
@@ -20,6 +34,18 @@ function Answers (props) {
 }
 
 function AnswerTable (props) {
+  const [sortOrder, setSortOrder] = useState('none');
+
+  const sortedAnswers = [...props.answers];
+  if(sortOrder === 'asc')
+    sortedAnswers.sort((a,b) => a.score - b.score);
+  else if (sortOrder == 'desc')
+    sortedAnswers.sort((a,b) => b.score - a.score);
+
+  const sortByScore = () => {
+    setSortOrder(oldOrder => oldOrder === 'asc' ? 'desc' : 'asc');
+  }
+
   return (
     <Table striped>
       <thead>
@@ -27,12 +53,12 @@ function AnswerTable (props) {
           <th>Date</th>
           <th>text</th>
           <th>Author</th>
-          <th>Score</th>
+          <th>Score <Button variant='link' onClick={sortByScore} style={{color: 'black'}}><i className={sortOrder ==='asc' ? 'bi bi-sort-numeric-up' : 'bi bi-sort-numeric-down'}></i></Button></th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        { props.answers.map((ans) => <AnswerRow answer={ans} key={ans.id} voteUp = {props.voteUp}/>) }
+        { sortedAnswers.map((ans) => <AnswerRow answer={ans} key={ans.id} voteUp={props.voteUp} handleEdit={props.handleEdit}/>) }
       </tbody>
     </Table>
   );
@@ -40,7 +66,7 @@ function AnswerTable (props) {
 
 function AnswerRow(props) {
   return(
-    <tr><AnswerData answer={props.answer}/><AnswerAction answersId={props.answer.id} voteUp = {props.voteUp} /></tr>
+    <tr><AnswerData answer={props.answer}/><AnswerAction answer={props.answer} voteUp={props.voteUp} handleEdit={props.handleEdit}/></tr>
   );
 }
 
@@ -55,11 +81,15 @@ function AnswerData(props) {
   );
 }
 
-function AnswerAction({ answersId, voteUp }) {
+function AnswerAction(props) {
   return(
     <td>
-      <Button variant='warning' onClick = {() => voteUp(answersId)}><i className='bi bi-arrow-up'></i></Button>
-      <Button variant='primary' className='mx-1'><i className='bi bi-pencil-square'></i></Button> 
+      <Button variant='warning' onClick={
+        () => props.voteUp(props.answer.id)
+      }><i className='bi bi-arrow-up'></i></Button>
+      <Button variant='primary' className='mx-1' onClick={
+        () =>props.handleEdit(props.answer)
+      }><i className='bi bi-pencil-square'></i></Button> 
       <Button variant='danger'><i className='bi bi-trash'></i></Button>
     </td>
   );
